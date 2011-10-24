cecc47c61a093ad535b6529e730571b87c0a9814
zbyhoo <zbyhoo@gmail.com>
2011-10-24 20:46:02 +0200

    Initial Commit

---
+++ WallyBall/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  WallyBall
+//
+//  Created by Zbigniew Kominek on 10/24/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
